3a5f8860794e10cbc9d51ce4367322b175af03ae
Jaxx4Fun <309329794@qq.com>
2024-02-28 16:31:04 +0800

    Fix tests of credential codec

--- tests/test_credential_codec.py
+++ tests/test_credential_codec.py
@@ -31,24 +31,36 @@ class TestEncodeDecode(TestCase):
         self.assertEqual(original_config, new_config)
         self.assertEqual(new_config.get("组", "键"), "值")
 
+class TestRealCredentials(TestCase):
+    def setUp(self):
+        self.credential_path = "./myConfig/credentials"
+        self.base64_credential_path = self.credential_path + ".base64"
+    def test_encode_decode_credentials(self):
+        """test if config after encoding and then decoding is the same
+        """
+        expected_credential_count = 6
+        config = ConfigParser()
+        with open(self.base64_credential_path, "r") as f:
+            decode(f.read(), config)
+        self.assertEqual(len(config.sections()), expected_credential_count, "每当添加一个手机号，expected_credential_count需要相应增加 1，以此确保新加的手机号没有因为误操作被遗漏")
 
-@skip("This test is like a tool to generate base64-encoded credential file of text credential")
+@skip("This test is like a tool to generate base64-encoded credential file of text credential, which you can run manually by comment this line out")
 class TestBase64ForRealFile(TestCase):
     def setUp(self):
         self.credential_path = "./myConfig/credentials"
         self.base64_credential_path = self.credential_path + ".base64"
     def test_encode_to_file(self):
-        cf = ConfigParser()
-        cf.read(self.credential_path, encoding='utf-8')
+        configs_from_origin = ConfigParser()
+        configs_from_origin.read(self.credential_path, encoding='utf-8')
         with open(self.base64_credential_path, "wb") as f:
-            encode(cf, f)
+            encode(configs_from_origin, f)
         with open(self.base64_credential_path, "r") as f:
             s = io.StringIO(f.read())
-            cp = ConfigParser()
-            decode(s.getvalue(), cp)
+            configs_from_b64 = ConfigParser()
+            decode(s.getvalue(), configs_from_b64)
             s.truncate(0)
-            cp.write(s)
-            print(s.getvalue())
+            configs_from_b64.write(s)
+        self.assertSetEqual(set(configs_from_b64.sections()), set(configs_from_origin.sections()))
 
 
 
